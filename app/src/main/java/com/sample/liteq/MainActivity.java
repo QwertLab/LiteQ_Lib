601e5013e2c69a42f5072a2d3c0ff38a2e80fa44
Qwertlab <ad.qwertlab@gmail.com>
2023-02-16 18:12:29 +0900

    Initial commit

---
+++ app/src/main/java/com/sample/liteq/MainActivity.java
@@ -0,0 +1,28 @@
+package com.sample.liteq;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.qwertlab.liteq.main.LiteQManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    /***
+     * API Document : https://api.qwertlab.com/liteq/index.php
+     * Admin : https://agent.qwertlab.com/
+     *
+     * @param savedInstanceState
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //-- LiteQ SDK Call ------------------------------------------------------------------------
+        LiteQManager.initLiteQ(MainActivity.this, "AgentKey");
+        //------------------------------------------------------------------------------------------
+
+    }
+}
